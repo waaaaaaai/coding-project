dc2505e05044b80903d1786e3201868414e337a9
waaaaaaai <wanyuaaaa@gmail.com>
2018-08-27 15:22:59 +0800

    initial commit

---
+++ vue.js/vueapp01/src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>
+      <ul>
+        <li v-for="user in users">
+          {{user.firstname}} {{user.lastname}}
+        </li>
+      </ul>
+  </div>
+    <div>
+      <input type="text" v-model="input_val">
+    </div>
+    <div>
+      Input Value: <g v-text="input_val"></g>
+    </div>
+  
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      users: [
+        {firstname: 'Sebastian', lastname: 'Eschweiler'},
+        {firstname: 'Bill', lastname: 'Smith'},
+        {firstname: 'John', lastname: 'Porter'}
+      ],
+      input_val : '',
+      counter : 0 
+    }
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
